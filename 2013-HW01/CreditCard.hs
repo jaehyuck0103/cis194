1159f4e1fd6011e95c3f923dbe8041074b8b9f96
Jae-Hyuck Park <jaehyuck0103@gmail.com>
2020-02-08 21:39:20 +0900

    Apply formatter.

--- 2013-HW01/CreditCard.hs
+++ 2013-HW01/CreditCard.hs
@@ -2,47 +2,45 @@
 
 -- Exercise 1
 toDigits :: Integer -> [Integer]
-toDigits x
-    | x < 1 = []
-    | otherwise = toDigits(x `div` 10) ++ [x `mod` 10]
+toDigits x | x < 1     = []
+           | otherwise = toDigits (x `div` 10) ++ [x `mod` 10]
 
 toDigitsRev :: Integer -> [Integer]
-toDigitsRev x
-    | x < 1 = []
-    | otherwise = (x `mod` 10) : toDigitsRev(x `div` 10)
+toDigitsRev x | x < 1     = []
+              | otherwise = (x `mod` 10) : toDigitsRev (x `div` 10)
 
 toDigitsRevV2 :: Integer -> [Integer]
-toDigitsRevV2 = reverse .toDigits
+toDigitsRevV2 = reverse . toDigits
 
 exercise1 :: IO ()
 exercise1 = do
-  print $ toDigits 1234 == [1,2,3,4]
-  print $ toDigitsRev 1234 == [4,3,2,1]
-  print $ toDigitsRevV2 1234 == [4,3,2,1]
+  print $ toDigits 1234 == [1, 2, 3, 4]
+  print $ toDigitsRev 1234 == [4, 3, 2, 1]
+  print $ toDigitsRevV2 1234 == [4, 3, 2, 1]
   print $ toDigits 0 == []
   print $ toDigits (-17) == []
 
 -- Exercise 2
 doubleEveryOtherFromLeft :: [Integer] -> [Integer]
-doubleEveryOtherFromLeft [] = []
-doubleEveryOtherFromLeft [x] = [x]
-doubleEveryOtherFromLeft (x:y:zs) = x : 2*y : doubleEveryOtherFromLeft zs
+doubleEveryOtherFromLeft []           = []
+doubleEveryOtherFromLeft [x         ] = [x]
+doubleEveryOtherFromLeft (x : y : zs) = x : 2 * y : doubleEveryOtherFromLeft zs
 
 doubleEveryOther :: [Integer] -> [Integer]
 doubleEveryOther = reverse . doubleEveryOtherFromLeft . reverse
 
 exercise2 :: IO ()
 exercise2 = do
-  print $ doubleEveryOther [8,7,6,5] == [16,7,12,5] 
-  print $ doubleEveryOther [1,2,3] == [1,4,3]
+  print $ doubleEveryOther [8, 7, 6, 5] == [16, 7, 12, 5]
+  print $ doubleEveryOther [1, 2, 3] == [1, 4, 3]
 
 -- Exercise 3
 sumDigits :: [Integer] -> Integer
-sumDigits [] = 0
-sumDigits (x:ys) = (sum $ toDigits x) + sumDigits ys
+sumDigits []       = 0
+sumDigits (x : ys) = (sum $ toDigits x) + sumDigits ys
 
 exercise3 :: IO ()
-exercise3 = print $ sumDigits [16,7,12,5] == 1 + 6 + 7 + 1 + 2 + 5
+exercise3 = print $ sumDigits [16, 7, 12, 5] == 1 + 6 + 7 + 1 + 2 + 5
 
 -- Exercise 4
 validate :: Integer -> Bool
@@ -56,7 +54,7 @@ exercise4 = do
 -- Main
 main :: IO ()
 main = do
-    exercise1
-    exercise2
-    exercise3
-    exercise4
+  exercise1
+  exercise2
+  exercise3
+  exercise4
--- 2013-HW01/Hanoi.hs
+++ 2013-HW01/Hanoi.hs
@@ -3,16 +3,19 @@
 type Peg = String
 type Move = (Peg, Peg)
 
+
 hanoi :: Integer -> Peg -> Peg -> Peg -> [Move]
 hanoi 1 src goal _ = [(src, goal)]
 hanoi n src goal tmp
-    | n < 1 = []
-    | otherwise = hanoi (n-1) src tmp goal ++ [(src, goal)] ++ hanoi (n-1) tmp goal src
+  | n < 1
+  = []
+  | otherwise
+  = hanoi (n - 1) src tmp goal ++ [(src, goal)] ++ hanoi (n - 1) tmp goal src
 
 
 -- Main
-main :: IO()
+main :: IO ()
 main = do
-    print $ hanoi 2 "a" "b" "c"
-    print $ hanoi 3 "a" "b" "c"
-    print (length (hanoi 15 "a" "b" "c") == 32767)
+  print $ hanoi 2 "a" "b" "c"
+  print $ hanoi 3 "a" "b" "c"
+  print (length (hanoi 15 "a" "b" "c") == 32767)
